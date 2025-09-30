78eee1a49bc247b5d260067dd601f81370a2d360
AbdurRob <abdurrobr34l@gmail.com>
2025-10-01 00:43:19 +0600

    Showed user detail dynamicly

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useLoaderData } from "react-router";
 
 const Users = () => {
-
   const users = useLoaderData();
   console.log(users);
 
@@ -11,14 +10,15 @@ const Users = () => {
       <h1 className="text-5xl">It is Users</h1>
 
       <div className="mt-10 grid grid-cols-4 gap-5">
-        {users.map(user =>
-        // {{name} = users }
-          <div className="p-3 border-2 rounded-xl">
-            <h2 className="font-black text-cyan-300">{user.name}</h2>
-            <p>{user.username}</p>
-            <p>{user.email}</p>
+        {users.map(({id, name, username, email, address: {zipcode}}) => (
+
+          <div key={id} className="p-3 border-2 rounded-xl">
+            <h2 className="font-black text-cyan-300">{name}</h2>
+            <p>{username}</p>
+            <p>{email}</p>
+            <p>{zipcode}</p>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
